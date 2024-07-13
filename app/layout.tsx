d6ef8398997b9612b5a4c7db8451a651622bed86
valpedro59 <valpedro59@gmail.com>
2024-07-13 12:41:02 +0100

    Whatsapp adding

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import WhatsappBtn from "@/components/ui/whatsapp";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <WhatsappBtn />
         <Footer />
       </body>
     </html>
